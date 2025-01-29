643c5b68b793d756940935c1140bb7182e397375
tankvaishali <vaishalitank28603@gmail.com>
2025-01-29 16:29:49 +0530

    set images of awards

--- src/Components/ProfilePage/Certificate.jsx
+++ src/Components/ProfilePage/Certificate.jsx
@@ -1,41 +1,62 @@
 import React from "react";
 const images = [
   {
-    img: require("../../Assets/Images/Awards/award1.jpg")
+    img: require("../../Assets/Images/Awards/award1.jpg"),
+    fit:"object-fit-fill",
   },
   {
-    img: require("../../Assets/Images/Awards/award2.jpg")
+    img: require("../../Assets/Images/Awards/award2.jpg"),
+    fit:"object-fit-fill",
   },
   {
-    img: require("../../Assets/Images/Awards/award3.jpg")
+    img: require("../../Assets/Images/Awards/award3.jpg"),
+    fit:"object-fit-fill",
   },
   {
-    img: require("../../Assets/Images/Awards/award4.jpg")
+    img: require("../../Assets/Images/Awards/award4.jpg"),
+    fit:"object-fit-fill",
+
   },
   {
-    img: require("../../Assets/Images/Awards/award5.jpg")
+    img: require("../../Assets/Images/Awards/award5.jpg"),
+    fit:"object-fit-fill",
+
   },
 
   {
-    img: require("../../Assets/Images/Awards/award6.jpg")
+    img: require("../../Assets/Images/Awards/award6.jpg"),
+    fit:"object-fit-contain",
+
   },
   {
-    img: require("../../Assets/Images/Awards/award7.jpg")
+    img: require("../../Assets/Images/Awards/award7.jpg"),
+    fit:"object-fit-cover",
+
   },
   {
-    img: require("../../Assets/Images/Awards/award8.jpg")
+    img: require("../../Assets/Images/Awards/award8.jpg"),
+    fit:"object-fit-contain",
+
   },
   {
-    img: require("../../Assets/Images/Awards/award9.jpg")
+    img: require("../../Assets/Images/Awards/award9.jpg"),
+    fit:"object-fit-cover",
+
   },
   {
-    img: require("../../Assets/Images/Awards/award10.jpg")
+    img: require("../../Assets/Images/Awards/award10.jpg"),
+    fit:"object-fit-cover",
+
   },
   {
-    img: require("../../Assets/Images/Awards/award11.jpg")
+    img: require("../../Assets/Images/Awards/award11.jpg"),
+    fit:"object-fit-contain",
+
   },
   {
-    img: require("../../Assets/Images/Awards/award12.jpg")
+    img: require("../../Assets/Images/Awards/award12.jpg"),
+    fit:"object-fit-cover",
+
   },
 ];
 
@@ -80,7 +101,7 @@ function Certificate() {
                 data-aos-once="true"
               >
                 <div className="certimain">
-                  <img src={src.img} className="certi-image img-fluid object-fit-fill" alt="" />
+                  <img src={src.img} className={`certi-image img-fluid ${src.fit}`} alt="" />
                 </div>
               </div>
             ))}
